2c3c1de84b50ce4a0d980cdfcdc285ff00168346
singularity014 <49594316+singularity014@users.noreply.github.com>
2021-01-20 20:11:56 +0530

    Update pre_processor.py

--- pre_processor/pre_processor.py
+++ pre_processor/pre_processor.py
@@ -1,6 +1,7 @@
 # import libs
 import numpy as np
 import pandas as pd
+
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
